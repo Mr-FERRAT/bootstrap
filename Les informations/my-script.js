3324fd8776acf286068e0693293d5fd22993ac36
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-01-30 19:58:42 +0100

    fin de la partition sur les tooltipé

--- Les informations/my-script.js
+++ Les informations/my-script.js
@@ -1,3 +1,5 @@
-
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(element => new bootstrap.Popover(element));
+
+const tooltipeTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipeList = [...tooltipeTriggerList].map(element => new bootstrap.Tooltip(element));
