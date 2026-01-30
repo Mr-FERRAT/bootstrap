7a53746f8d664ce32aaef09021e42ddc8019d96b
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-01-30 22:18:39 +0100

    fin de la partition sur les toast et la fin de la section aussi

--- Les informations/my-script.js
+++ Les informations/my-script.js
@@ -3,3 +3,10 @@ const popoverList = [...popoverTriggerList].map(element => new bootstrap.Popover
 
 const tooltipeTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipeList = [...tooltipeTriggerList].map(element => new bootstrap.Tooltip(element));
+
+const toastTrigger = document.getElementById("my-toast");
+const toastContent = document.getElementById("my-toast-content");
+const toast = new bootstrap.Toast(toastContent, {delay: 1500, animation: true});
+toastTrigger.addEventListener('click', () => {
+    toast.show();
+});
