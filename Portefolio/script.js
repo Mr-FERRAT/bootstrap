4e5692b981861be86d12c4020b513d45baf57ed9
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-08-14 22:00:10 +0100

    Fin de la section du formulaire de contact

--- Portefolio/script.js
+++ Portefolio/script.js
@@ -1,33 +1,17 @@
-//Envoyer le mail
-function sendMail(event) {
+// Envoyer le mail
+function sendMail (event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
-    const mail = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    const name = document.getElementById('name'),value;
+    const mail = document.getElementById('email'),value;
+    const message = document.getElementById('message'),value;
 
-    const subject = encodeURIComponent('Nouveau message de ' + name + '\nEcrit par: ' + mail);
-    const body = encodeURIComponent('Sujet du message: \n\n' + message);
-    const mailToLink = 'mailto:info@codabee.com?subject=' + subject + '&body=' + body;
+    const subject = encodeURIComponent('Nouveau message de '+name +'\nEcrit par: ' + mail);
+    const body = encodeURIComponent('Sujet du message: \n\n'+ message);
+    const mailToLink = 'mawloud.ferrat@gmail.com?subject=' + subject + '&body=' +body;
     window.location.href = mailToLink;
 }
 
-function closeMenu(event) {
-    const collapse = document.querySelector('.navbar-collapse');
-    if (collapse.classList.contains('show')) {
-        new bootstrap.Collapse(collapse).hide();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    //envoi  de mail
-    const form = document.getElementById('contactForm');
+document.addEventListener('DOMContentLoaded', function (){
+    const form = document.getElementById('constactForm');
     form.addEventListener('submit', sendMail);
-
-    //Fermer le menu
-    const navList = document.querySelectorAll('.navbar-nav a');
-    navList.forEach(nav => {
-        nav.addEventListener('click', closeMenu);
-    })
-        
-
-});
+})
