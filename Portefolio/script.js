d86ad5716785e110e5594aab1526f352ad2fb9f2
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-08-14 23:32:32 +0100

    bug est fixee

--- Portefolio/script.js
+++ Portefolio/script.js
@@ -1,11 +1,11 @@
 // Envoyer le mail
 function sendMail (event) {
     event.preventDefault();
-    const name = document.getElementById('name'),value;
-    const mail = document.getElementById('email'),value;
-    const message = document.getElementById('message'),value;
+    const name = document.getElementById('name').value;
+    const mail = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
-    const subject = encodeURIComponent('Nouveau message de '+name +'\nEcrit par: ' + mail);
+    const subject = encodeURIComponent('Nouveau message de '+ name +'\nEcrit par: ' + mail);
     const body = encodeURIComponent('Sujet du message: \n\n'+ message);
     const mailToLink = 'mawloud.ferrat@gmail.com?subject=' + subject + '&body=' +body;
     window.location.href = mailToLink;
